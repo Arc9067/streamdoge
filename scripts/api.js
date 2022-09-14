a4f5094aead53ba3fa1334f13b5e270c5912253a
Arc9067 <80489007+Arc9067@users.noreply.github.com>
2022-09-14 19:36:43 +0000

    Add files via upload

--- scripts/api.js
+++ scripts/api.js
@@ -1,10 +1,15 @@
 
 async function a() {
-    const response = await fetch("https://api.pancakeswap.info/api/v2/tokens/0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82")
+    const response = await fetch("https://api.pancakeswap.info/api/v2/tokens/0x7130d2a12b9bcbfae4f2634d864a1ee1ce3ead9c")
     const output = await response.json()
     const {updated_at,data} = output
     const {name,symbol,price,price_BNB} = data
-    console.log(symbol)
+    
+    const tkname = document.getElementById("tkname").innerText=name
+    const tksymbol = document.getElementById("tksymbol").innerText=symbol
+    const tkprice = document.getElementById("tkprice").innerText=`$${Math.floor(price)}`
+    const tkbnb = document.getElementById("tkbnb").innerText=`BNB ${Math.round(price_BNB)}`
+
 }
 
 // const run = setTimeout(a,)
